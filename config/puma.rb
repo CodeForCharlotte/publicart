959f6c36e14999f3cde90c7025f180709d2f56fb
Jim Van Fleet <jim.van.fleet@levvel.io>
2017-08-02 09:48:11 -0400

    Initial commit

---
+++ config/puma.rb
@@ -0,0 +1,19 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+if ENV["BOXEN_SOCKET_DIR"]
+  bind        "unix://#{ENV['BOXEN_SOCKET_DIR']}/public-art"
+else
+  port        ENV['PORT']     || 3000
+end
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
